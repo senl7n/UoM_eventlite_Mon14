f161e3fb236d25fb320d7e59279ac2b47c88b205
chaSenecHo <101160308+chaSenecHo@users.noreply.github.com>
2023-02-13 18:22:58 +0000

    Persist Events
    Add annotations to annotate the Event class and its fields
    for persistence, create an EventRepository interface in
    the dao package and wire it into the class that implements
    the EventService interface.
    Add a save method to the EventService interface.
    
    Change: Event.java, EventRepository.java, EventService.java
    EventServiceImpl.java

--- src/main/java/uk/ac/man/cs/eventlite/dao/EventServiceImpl.java
+++ src/main/java/uk/ac/man/cs/eventlite/dao/EventServiceImpl.java
@@ -1,17 +1,10 @@
 package uk.ac.man.cs.eventlite.dao;
 
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import uk.ac.man.cs.eventlite.entities.Event;
 
@@ -22,34 +15,19 @@ public class EventServiceImpl implements EventService {
 
 	private final static String DATA = "data/events.json";
 
-	@Override
-	public long count() {
-		long count = 0;
-		Iterator<Event> i = findAll().iterator();
-
-		for (; i.hasNext(); count++) {
-			i.next();
-		}
-
-		return count;
+    @Autowired
+    private EventRepository eventRepository;
+    public long count() {
+        return eventRepository.count();
 	}
 
-	@Override
+    @Override
 	public Iterable<Event> findAll() {
-		ArrayList<Event> events = new ArrayList<Event>();
-
-		try {
-			ObjectMapper mapper = new ObjectMapper();
-			mapper.registerModule(new JavaTimeModule());
-
-			InputStream in = new ClassPathResource(DATA).getInputStream();
-
-			events = mapper.readValue(in, mapper.getTypeFactory().constructCollectionType(List.class, Event.class));
-		} catch (Exception e) {
-			log.error("Exception while reading file '" + DATA + "': " + e);
-			// If we can't read the file, then the event list is empty...
-		}
-
-		return events;
+		return eventRepository.findAll();
 	}
+
+    @Override
+    public Event save(Event event) {
+        return eventRepository.save(event);
+    }
 }

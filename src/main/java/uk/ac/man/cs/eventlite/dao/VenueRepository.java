13b14c5c6908a636dc44f6645c4150c7a4ac53d7
Afnan Bokhari <Afnan.bokhari@student.manchester.ac.uk>
2023-02-14 17:30:32 +0000

    Persist Venue
    Add annotations for Venue class
    for persistence,and create an VenueRepository and wire it into the class
    that implements
    the VenueService.
    Add a save method to the VenueService.
    
    Change: Venue.java, VenueRepository.java, VenueService.java
    VenueServiceImpl.java

--- src/main/java/uk/ac/man/cs/eventlite/dao/VenueRepository.java
+++ src/main/java/uk/ac/man/cs/eventlite/dao/VenueRepository.java
@@ -1,13 +1,13 @@
 package uk.ac.man.cs.eventlite.dao;
 
 import uk.ac.man.cs.eventlite.entities.Venue;
+import org.springframework.data.repository.CrudRepository;
 
-public interface VenueService  {
-
+public interface VenueRepository extends CrudRepository<Venue, Long>
+{
 	public long count();
 
 	public Iterable<Venue> findAll();
-	
 	public Venue save(Venue venue);
+	
 }
- 

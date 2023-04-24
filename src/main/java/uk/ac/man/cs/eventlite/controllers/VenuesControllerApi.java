5194441ecb4c5021436b8c64736f264f0cea1079
Renhao Bao <renhao.bao@student.manchester.ac.uk>
2023-04-24 13:54:37 +0100

    show the required infomation, but still with invalid links

--- src/main/java/uk/ac/man/cs/eventlite/controllers/VenuesControllerApi.java
+++ src/main/java/uk/ac/man/cs/eventlite/controllers/VenuesControllerApi.java
@@ -12,6 +12,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import uk.ac.man.cs.eventlite.entities.Event;
 import uk.ac.man.cs.eventlite.assemblers.VenueModelAssembler;
 import uk.ac.man.cs.eventlite.dao.VenueService;
 import uk.ac.man.cs.eventlite.entities.Venue;
@@ -37,13 +38,28 @@ public class VenuesControllerApi {
 
     @GetMapping("/{id}")
     public EntityModel<Venue> getVenue(@PathVariable("id") long id) {
-        throw new VenueNotFoundException(id);
+        Venue venue = venueService.findById(id).orElseThrow(() -> new VenueNotFoundException(id));
+        return venueAssembler.toModel(venue);
     }
+
 
     @GetMapping
     public CollectionModel<EntityModel<Venue>> getAllVenues() {
         return venueAssembler.toCollectionModel(venueService.findAll())
                 .add(linkTo(methodOn(VenuesControllerApi.class).getAllVenues()).withSelfRel());
     }
+
+    @GetMapping("/{id}/events")
+    public CollectionModel<EntityModel<Event>> getVenueEvents(@PathVariable("id") long id) {
+        // Your implementation here
+    }
+
+    @GetMapping("/{id}/next3events")
+    public CollectionModel<EntityModel<Event>> getVenueNext3Events(@PathVariable("id") long id) {
+        // Your implementation here
+    }
+
+    
+
 
 }

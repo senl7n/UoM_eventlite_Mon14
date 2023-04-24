89f9a6c99f8fcb00e55c55a290f365b6feedeca8
Renhao Bao <renhao.bao@student.manchester.ac.uk>
2023-04-24 16:39:51 +0100

    enable the functionality of most links

--- src/main/java/uk/ac/man/cs/eventlite/controllers/VenuesControllerApi.java
+++ src/main/java/uk/ac/man/cs/eventlite/controllers/VenuesControllerApi.java
@@ -12,11 +12,20 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import uk.ac.man.cs.eventlite.assemblers.EventModelAssembler;
+import uk.ac.man.cs.eventlite.dao.EventService;
 import uk.ac.man.cs.eventlite.entities.Event;
 import uk.ac.man.cs.eventlite.assemblers.VenueModelAssembler;
 import uk.ac.man.cs.eventlite.dao.VenueService;
 import uk.ac.man.cs.eventlite.entities.Venue;
 import uk.ac.man.cs.eventlite.exceptions.VenueNotFoundException;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @RestController
 @RequestMapping(value = "/api/venues", produces = { MediaType.APPLICATION_JSON_VALUE, MediaTypes.HAL_JSON_VALUE })
@@ -26,6 +35,9 @@ public class VenuesControllerApi {
 
     @Autowired
     private VenueService venueService;
+
+    @Autowired
+    private EventService eventService;
 
     @Autowired
     private VenueModelAssembler venueAssembler;
@@ -46,20 +58,70 @@ public class VenuesControllerApi {
     @GetMapping
     public CollectionModel<EntityModel<Venue>> getAllVenues() {
         return venueAssembler.toCollectionModel(venueService.findAll())
-                .add(linkTo(methodOn(VenuesControllerApi.class).getAllVenues()).withSelfRel());
+                .add(linkTo(methodOn(VenuesControllerApi.class).getAllVenues()).withSelfRel())
+                .add(linkTo(methodOn(VenuesControllerApi.class).getProfile()).withRel("profile"));
     }
+
+    @GetMapping("/profile/venues")
+    public ResponseEntity<?> getProfile() {
+        return null;
+    }
+
 
     @GetMapping("/{id}/events")
-    public CollectionModel<EntityModel<Event>> getVenueEvents(@PathVariable("id") long id) {
-        // Your implementation here
+    public ResponseEntity<CollectionModel<EntityModel<Event>>> getVenueEvents(@PathVariable("id") long id) {
+        Venue venue = venueService.findById(id).orElseThrow(() -> new VenueNotFoundException(id));
+        Iterable<Event> allEvents = eventService.findUpcomingEvents();
+        List<Event> eventsAtVenue = new ArrayList<>();
+
+        for (Event event : allEvents) {
+            if (event.getVenue().equals(venue)) {
+                eventsAtVenue.add(event);
+            }
+        }
+
+        List<EntityModel<Event>> eventModels = new ArrayList<>();
+        for (Event event : eventsAtVenue) {
+            EntityModel<Event> eventModel = EntityModel.of(event,
+                    linkTo(methodOn(EventsControllerApi.class).getEvent(event.getId())).withSelfRel());
+
+            eventModels.add(eventModel);
+        }
+
+        CollectionModel<EntityModel<Event>> eventCollectionModel = CollectionModel.of(eventModels,
+                linkTo(methodOn(VenuesControllerApi.class).getVenueEvents(id)).withSelfRel());
+
+        return ResponseEntity.ok(eventCollectionModel);
     }
+
 
     @GetMapping("/{id}/next3events")
-    public CollectionModel<EntityModel<Event>> getVenueNext3Events(@PathVariable("id") long id) {
-        // Your implementation here
+    public ResponseEntity<CollectionModel<EntityModel<Event>>> getVenueNext3Events(@PathVariable("id") long id) {
+        Venue venue = venueService.findById(id).orElseThrow(() -> new VenueNotFoundException(id));
+        Iterable<Event> allEvents = eventService.findUpcomingEvents();
+        List<Event> eventsAtVenue = new ArrayList<>();
+
+        for (Event event : allEvents) {
+            if (event.getVenue().equals(venue)) {
+                eventsAtVenue.add(event);
+            }
+        }
+
+        // Sort the events by date and get the next 3 events
+        eventsAtVenue.sort(Comparator.comparing(Event::getDate));
+        List<Event> next3Events = eventsAtVenue.stream().limit(3).collect(Collectors.toList());
+
+        List<EntityModel<Event>> eventModels = new ArrayList<>();
+        for (Event event : next3Events) {
+            EntityModel<Event> eventModel = EntityModel.of(event,
+                    linkTo(methodOn(EventsControllerApi.class).getEvent(event.getId())).withSelfRel());
+
+            eventModels.add(eventModel);
+        }
+
+        CollectionModel<EntityModel<Event>> eventCollectionModel = CollectionModel.of(eventModels,
+                linkTo(methodOn(VenuesControllerApi.class).getVenueNext3Events(id)).withSelfRel());
+
+        return ResponseEntity.ok(eventCollectionModel);
     }
-
-    
-
-
 }

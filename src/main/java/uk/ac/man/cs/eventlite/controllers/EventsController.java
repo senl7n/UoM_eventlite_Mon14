ef929360a2f04e84c59982dcdab926a3b0e6f9a7
Xinyu Li <sophiaxyli@163.com>
2023-04-26 15:29:14 +0100

    Update token
    Change: EventsController.java

--- src/main/java/uk/ac/man/cs/eventlite/controllers/EventsController.java
+++ src/main/java/uk/ac/man/cs/eventlite/controllers/EventsController.java
@@ -273,7 +273,7 @@ public class EventsController {
 
     @PostMapping("/postComment/{id}")
     public String postComment(@PathVariable("id") long id, @RequestParam("comment") String comment, Model model, RedirectAttributes redirectAttributes) throws Mastodon4jRequestException {
-        String accessToken = "LdEzyxYey4tCzNvk4sxzAWqOFrCwg21iMmlie8YkFtA";
+        String accessToken = "AQHbd7AEwgaFHDARmaYPSPjkIvFrIMu-ZWhnpU2AIN0";
         MastodonClient mastodonClient = new MastodonClient.Builder("mastodon.social", new OkHttpClient.Builder(), new Gson())
                 .accessToken(accessToken)
                 .useStreamingApi()

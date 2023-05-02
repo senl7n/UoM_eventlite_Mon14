56b5825d7aa70dadc221bf2a9e698832401f3f85
Renhao Bao <renhao.bao@student.manchester.ac.uk>
2023-05-02 18:20:57 +0100

    update the TestDataLoader, and add some tests on VenueService and EventService

--- src/test/java/uk/ac/man/cs/eventlite/dao/EventServiceTest.java
+++ src/test/java/uk/ac/man/cs/eventlite/dao/EventServiceTest.java
@@ -1,8 +1,11 @@
 package uk.ac.man.cs.eventlite.dao;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -16,29 +19,81 @@ import uk.ac.man.cs.eventlite.EventLite;
 import uk.ac.man.cs.eventlite.entities.Event;
 import uk.ac.man.cs.eventlite.entities.Venue;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = EventLite.class)
 @DirtiesContext
 @ActiveProfiles("test")
-//@Disabled
+
 public class EventServiceTest extends AbstractTransactionalJUnit4SpringContextTests {
 
 	@Autowired
-	private MockMvc mvc;
+	private EventService EventServiceImpl;
 
-	@Mock
-	private Event event;
+	@Autowired
+	private VenueService VenueServiceImpl;
 
-	@Mock
-	private Venue venue;
+	public void setup() {
+		MockitoAnnotations.openMocks(this);
+	}
 
-	@MockBean
-	private EventService eventService;
+	@Test
+	public void testFindAll() {
+		// Retrieve all events using the findAll() method
+		Iterable<Event> allEvents = EventServiceImpl.findAll();
+		int eventCount = 0;
 
-	@MockBean
-	private VenueService venueService;
+		// Iterate through all events and increment the counter for each event
+		for (Event event : allEvents) {
+			eventCount++;
+		}
 
-	// This class is here as a starter for testing any custom methods within the
-	// EventService. Note: It is currently @Disabled!
+		// Compare the event count with the expected value
+		assertEquals(6, eventCount, "Expected to find 6 events in the repository");
+	}
+
+	@Test
+	public void testFindUpcomingEvents(){
+		Iterable<Event> upcomingEvents = EventServiceImpl.findUpcomingEvents();
+
+//		Iterate through the upcomingEvents and make sure that they are in the future
+		LocalDate currentDate = LocalDate.now();
+		LocalTime currentTime = LocalTime.now();
+		for (Event event : upcomingEvents) {
+			assertTrue(event.getDate().isAfter(currentDate) || (event.getDate().isEqual(currentDate) && event.getTime().isAfter(currentTime)));
+		}
+
+	}
+
+	@Test
+	public void testFindPreviousEvents(){
+		Iterable<Event> previousEvents = EventServiceImpl.findPreviousEvents();
+
+//		Iterate through the previousEvents and make sure that they are in the past
+		LocalDate currentDate = LocalDate.now();
+		LocalTime currentTime = LocalTime.now();
+		for (Event event : previousEvents) {
+			assertTrue(event.getDate().isBefore(currentDate) || (event.getDate().isEqual(currentDate) && event.getTime().isBefore(currentTime)));
+		}
+
+	}
+
+	@Test
+	public void testFindByNameContainingIgnoreCase() {
+		Iterable<Event> foundEvents = EventServiceImpl.findByNameContainingIgnoreCase("show");
+
+//      Iterate through the foundEvents and make sure that all of them contain the search keyword
+		for (Event event : foundEvents) {
+			assertTrue(event.getName().toLowerCase().contains("show".toLowerCase()));
+		}
+	}
+
+
+
 }
--- src/test/java/uk/ac/man/cs/eventlite/dao/VenueServiceTest.java
+++ src/test/java/uk/ac/man/cs/eventlite/dao/VenueServiceTest.java
@@ -1,7 +1,9 @@
 package uk.ac.man.cs.eventlite.dao;
 
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
@@ -10,17 +12,65 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 
 import uk.ac.man.cs.eventlite.EventLite;
+import uk.ac.man.cs.eventlite.entities.Event;
+import uk.ac.man.cs.eventlite.entities.Venue;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = EventLite.class)
 @DirtiesContext
 @ActiveProfiles("test")
-@Disabled
+
 public class VenueServiceTest extends AbstractTransactionalJUnit4SpringContextTests {
 
 	@Autowired
-	private VenueService venueService;
+	private EventService EventServiceImpl;
 
-	// This class is here as a starter for testing any custom methods within the
-	// VenueService. Note: It is currently @Disabled!
+	@Autowired
+	private VenueService VenueServiceImpl;
+
+
+	public void setup() {
+		MockitoAnnotations.openMocks(this);
+	}
+
+	@Test
+	public void testFindAll() {
+		// Retrieve all events using the findAll() method
+		Iterable<Venue> allVenues = VenueServiceImpl.findAll();
+		int venueCount = 0;
+
+		// Iterate through all events and increment the counter for each event
+		for (Venue venue : allVenues) {
+			venueCount++;
+		}
+
+		// Compare the event count with the expected value
+		assertEquals(3, venueCount, "Expected to find 3 venues in the repository");
+	}
+
+	@Test
+	public void testFindByNameContainingIgnoreCase() {
+		Iterable<Venue> foundVenues = VenueServiceImpl.findByNameContainingIgnoreCase("red");
+
+//      Iterate through the foundEvents and make sure that all of them contain the search keyword
+		for (Venue venue : foundVenues) {
+			assertTrue(venue.getName().toLowerCase().contains("red".toLowerCase()));
+		}
+	}
+
+	@Test
+	public void testFindByID() {
+		long expectedId = 1;
+		Optional<Venue> foundVenue = VenueServiceImpl.findById(expectedId);
+
+//      Iterate through the foundEvents and make sure that all of them contain the search keyword
+
+		assertTrue(foundVenue.isPresent(), "Expected venue not found");
+		assertEquals(expectedId, foundVenue.get().getId(), "Found venue has a different ID than expected");
+	}
 }

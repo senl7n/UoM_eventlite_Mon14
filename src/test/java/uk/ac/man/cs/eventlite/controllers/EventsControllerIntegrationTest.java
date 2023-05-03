5ce87b3f35fabd05eccf1344e33db164e35a48f7
Xinyu Li <sophiaxyli@163.com>
2023-05-03 22:30:02 +0100

    Add testDeleteEvent
    Create login and getCsrfToken functions to get
    the CSRF for admin login.
    
    Change: EventsControllerIntegrationTest.java

--- src/test/java/uk/ac/man/cs/eventlite/controllers/EventsControllerIntegrationTest.java
+++ src/test/java/uk/ac/man/cs/eventlite/controllers/EventsControllerIntegrationTest.java
@@ -1,11 +1,19 @@
 package uk.ac.man.cs.eventlite.controllers;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.core.StringContains.containsString;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.security.config.Elements.CSRF;
+import static org.springframework.web.reactive.function.client.ExchangeFilterFunctions.basicAuthentication;
+import static uk.ac.man.cs.eventlite.testutil.FormUtil.getCsrfToken;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.MediaType;
@@ -14,9 +22,17 @@ import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import uk.ac.man.cs.eventlite.EventLite;
+import uk.ac.man.cs.eventlite.dao.EventRepository;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = EventLite.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -27,7 +43,12 @@ public class EventsControllerIntegrationTest extends AbstractTransactionalJUnit4
 	@LocalServerPort
 	private int port;
 
+    @Autowired
+    private EventRepository eventRepository;
+
 	private WebTestClient client;
+    private final static String SESSION_KEY = "JSESSIONID";
+    private final static String LOGIN_PATH = "/sign-in";
 
 	@BeforeEach
 	public void setup() {
@@ -46,4 +67,39 @@ public class EventsControllerIntegrationTest extends AbstractTransactionalJUnit4
 					assertThat(result.getResponseBody(), containsString("99"));
 				});
 	}
+
+    private String[] login() {
+        String[] tokens = new String[2];
+
+        EntityExchangeResult<String> result = client.mutate().filter(basicAuthentication("Rob", "Haines")).build().get()
+                .uri("/events").accept(MediaType.TEXT_HTML).exchange().expectBody(String.class).returnResult();
+        tokens[0] = getCsrfToken(result.getResponseBody());
+        tokens[1] = result.getResponseCookies().getFirst(SESSION_KEY).getValue();
+
+        return tokens;
+    }
+
+    private String getCsrfToken(String body) {
+        Pattern pattern = Pattern.compile("(?s).*name=\"_csrf\".*?value=\"([^\"]+).*");
+        Matcher matcher = pattern.matcher(body);
+        assertTrue(matcher.matches());
+        return matcher.group(1);
+    }
+
+    @Test
+    public void testDeleteEvent() throws Exception {
+        String[] tokens = login();
+
+        // Then, attempt to delete an event using the authenticated session cookie and CSRF token.
+        MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
+        formData.add("_csrf", tokens[0]);
+
+        client.mutate().filter(basicAuthentication("Rob", "Haines")).build().delete().uri("/events/1")
+                .accept(MediaType.TEXT_HTML).exchange().expectStatus().isFound().expectBody(String.class);
+
+        // Finally, check that the event has been deleted.
+        client.get().uri("/events/1").accept(MediaType.TEXT_HTML).exchange().expectStatus().isNotFound();
+
+    }
+
 }

403ba7b73c4554c2de253e17ec087b84708b8b28
Shuo Li <shuo.li-7@student.manchester.ac.uk>
2023-05-05 00:02:32 +0100

    test setDescribtion

--- src/test/java/uk/ac/man/cs/eventlite/dao/EventServiceTest.java
+++ src/test/java/uk/ac/man/cs/eventlite/dao/EventServiceTest.java
@@ -1,6 +1,7 @@
 package uk.ac.man.cs.eventlite.dao;
 
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,19 +9,29 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import uk.ac.man.cs.eventlite.EventLite;
+import uk.ac.man.cs.eventlite.entities.Event;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = EventLite.class)
 @DirtiesContext
 @ActiveProfiles("test")
-@Disabled
+//@Disabled
 public class EventServiceTest extends AbstractTransactionalJUnit4SpringContextTests {
 
 	@Autowired
 	private EventService eventService;
-
+	
+	@Test
+	public void testSetDescribtion() {
+		Event event = new Event();
+		String expected_Describtion = "Hello this is a test";
+		event.setDescription("Hello this is a test");
+		String actual_Describtion = event.getDescription();
+		assertEquals(expected_Describtion, actual_Describtion);
+	}
 	// This class is here as a starter for testing any custom methods within the
 	// EventService. Note: It is currently @Disabled!
 }

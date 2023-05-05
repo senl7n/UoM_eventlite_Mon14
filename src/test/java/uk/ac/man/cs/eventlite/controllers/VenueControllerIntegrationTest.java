33d19dba1ffdcf0adfdd92b337545174c2a816f2
p80873ab <afnan.bokhari@student.manchester.ac.uk>
2023-05-05 02:02:15 +0100

    setup integratin test for vrnue controller

---
+++ src/test/java/uk/ac/man/cs/eventlite/controllers/VenueControllerIntegrationTest.java
@@ -0,0 +1,51 @@
+package uk.ac.man.cs.eventlite.controllers;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+
+import uk.ac.man.cs.eventlite.EventLite;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+
+import static org.hamcrest.Matchers.endsWith;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.core.StringContains.containsString;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = EventLite.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
+@ActiveProfiles("test")
+
+public class VenueControllerIntegrationTest extends AbstractTransactionalJUnit4SpringContextTests {
+	
+	@LocalServerPort
+	private int port;
+
+	private WebTestClient client;
+
+	@BeforeEach
+	public void setup() {
+		client = WebTestClient.bindToServer().baseUrl("http://localhost:" + port + "/api").build();
+	}
+
+}
